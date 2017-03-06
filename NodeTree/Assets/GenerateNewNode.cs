e5e47bc33cae6a834a613e996151bf340fa7c69d
Won Yong Ha <hpmsora@gmail.com>
2017-03-05 19:55:22 -0500

    5 March 2017

---
+++ NodeTree/Assets/GenerateNewNode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateNewNode : MonoBehaviour {
+
+	public GameObject node;
+
+	private GameObject newNode;
+
+	public void Generate() {
+		Debug.Log ("Generated!");
+		newNode = Instantiate(node, new Vector3(transform.position.x + 100, transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
+	}
+
+	/*void update() {
+		if(Input.GetKeyDown("d")) {
+			newNode = Instantiate(node, transform.position, Quaternion.identity) as GameObject;
+		}
+	}*/
+}
