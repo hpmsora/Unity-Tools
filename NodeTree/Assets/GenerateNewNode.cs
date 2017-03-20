cd6e3aac28b9b83aa8181d66962f59681dd63157
Won Yong Ha <hpmsora@gmail.com>
2017-03-20 12:55:56 -0400

    20 March 2017

--- NodeTree/Assets/GenerateNewNode.cs
+++ NodeTree/Assets/GenerateNewNode.cs
@@ -9,58 +9,66 @@ public class GenerateNewNode : MonoBehaviour {
 	public GameObject node;
 
 	private GameObject newNode;
-	private float nodeTrim;
+	private int nodeTrim = 50;
+
+	void Start() {
+		button.GetComponent<Button> ().interactable = true;
+	}
 
 	public void Generate() {
 		Debug.Log ("Generated!");
 
-		//int numberOfNode = Random.Range (1, 4);
-		int numberOfNode = 100;
+		int numberOfNode = Random.Range (1, 4);
 
-		double[,] positions = new double[numberOfNode,2];
+		Debug.Log (numberOfNode + " Generating");
+
+		float[,] positions = new float[numberOfNode,2];
+
+		int order = Mathf.Abs((int)transform.position.x) / nodeTrim;
+
+		order++;
+
+		//Button unpressable
+		if (button.GetComponent<Button> ().IsInteractable () == true) {
+			button.GetComponent<Button> ().interactable = false;
+			//button.GetComponent<Renderer> ().material.color = Color.black;
+		} else {
+			button.GetComponent<Button> ().interactable = true;
+			//button.GetComponent<Renderer> ().material.color = Color.white;
+		}
 
 		for (int i = 0; i < numberOfNode; i++) {
 			int x, y;
 			if (Random.Range (0, 2) == 0) {
-				x = Random.Range (1, 300);
+				x = Random.Range (1, 100) + nodeTrim * order;
 			} else {
-				x = Random.Range (-300, -1);
+				x = Random.Range (-100, -1) - nodeTrim * order;
 			}
 			if (Random.Range (0, 2) == 0) {
-				y = Random.Range (1, 300);
+				y = Random.Range (1, 100) + nodeTrim * order;
 			} else {
-				y = Random.Range (-300, -1);
+				y = Random.Range (-100, -1) - nodeTrim * order;
 			}
 			if (isNodeContainOkay (positions, x, y, i)) {
 				positions[i,0] = x;
 				positions[i,1] = y;
 			} else {
-				Debug.Log ("G :" +isNodeContainOkay (positions, x, y, i));
-				positions[i,0] = 0;
-				positions[i,1] = 0;
+				Debug.Log ("Again");
+				i--;
 			}
 		}
 
 		for (int i = 0; i < numberOfNode; i++) {
 			Debug.Log (positions[i,0]+" : " + positions[i,1]);
-		}
 
-		while (numberOfNode > 0) {
-			newNode = Instantiate (node, new Vector3 (transform.position.x + nodeTrim, transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
-			if (button.GetComponent<Button> ().IsInteractable () == true) {
-				button.GetComponent<Button> ().interactable = false;
-				button.GetComponent<Renderer> ().material.color = Color.gray;
-			} else {
-				button.GetComponent<Button> ().interactable = true;
-				button.GetComponent<Renderer> ().material.color = Color.white;
-			}
-			numberOfNode++;
+			newNode = Instantiate (node, new Vector3 (transform.position.x + positions [i, 0], transform.position.y + positions [i, 1], transform.position.z), Quaternion.identity) as GameObject;
+			Debug.Log ("Generate " + i);
 		}
 	}
 
-	public bool isNodeContainOkay(double[,] arrays, int x, int y, int num) {
+	public bool isNodeContainOkay(float[,] arrays, int x, int y, int num) {
 		for (int i = 0; i < num; i++) {
-			if ((x < (arrays[i,0] + 50) && x > (arrays[i,0] - 50)) || (y < (arrays[i,1] + 50) || y > (arrays[i,1] - 50))) {
+			if ((x < (arrays[i,0] + 50) && x > (arrays[i,0] - 50)) && (y < (arrays[i,1] + 50) || y > (arrays[i,1] - 50))) {
 				return false;
 			}
 		}
